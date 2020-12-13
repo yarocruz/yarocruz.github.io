e63563aefc6bd1bf2573d8861f4aa843083501db
yarocruz <yarocruz@gmail.com>
2020-12-12 22:56:46 -0500

    installed clean css to minify the huge tailwind file

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,12 @@
+const CleanCSS = require("clean-css");
+
 module.exports = function (config) {
+
+    // Minify CSS
+    config.addFilter("cssmin", function(code) {
+        return new CleanCSS({}).minify(code).styles;
+    });
+
     config.addPassthroughCopy("images") // makes sure that these files get outputed
     config.addPassthroughCopy("admin")
     config.addPassthroughCopy("assets/css")
