e4fdc0bef73fde026d195beda389f8ebc69be7c5
yarocruz <yarocruz@gmail.com>
2020-12-14 10:17:10 -0500

    added purge-css

--- postcss.config.js
+++ postcss.config.js
@@ -2,8 +2,9 @@ module.exports = {
     plugins: [
         require('tailwindcss')('tailwind.config.js'),
         require('autoprefixer'),
-        ...(process.env.NODE_ENV === "production"
-            ? [require(`postcss-clean`)]
-            : []),
+        require('@fullhuman/postcss-purgecss')({
+            content: ["./_site/**/*.html"],
+            default: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+        })
     ]
 }
