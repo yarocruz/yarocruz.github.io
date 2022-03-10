e38a9afc65ac34d490e68d64957deaf9ec99f529
yarocruz <yarocruz@gmail.com>
2022-03-09 19:00:22 -0500

    the site finally has a feed

--- .eleventy.js
+++ .eleventy.js
@@ -2,21 +2,19 @@ const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 const slugify = require("slugify");
 const htmlmin = require("html-minifier");
 const pluginRss = require("@11ty/eleventy-plugin-rss");
+const xmlFiltersPlugin = require('eleventy-xml-plugin');
 
 const { DateTime } = require("luxon");
 
-module.exports = function (config) {
+module.exports = function(config) {
     // Rss Plugin
-    config.addPlugin(pluginRss);
-
+    config.addPlugin(pluginRss, {
+        posthtmlRenderOptions: {}
+    });
+    // xml Plugin
+    config.addPlugin(xmlFiltersPlugin);
     // Eleventy Navigation https://www.11ty.dev/docs/plugins/navigation/
     config.addPlugin(eleventyNavigationPlugin);
-
-    // Configuration API: use eleventyConfig.addLayoutAlias(from, to) to add
-    // layout aliases! Say you have a bunch of existing content using
-    // layout: post. If you don’t want to rewrite all of those values, just map
-    // post to a new file like this:
-    // eleventyConfig.addLayoutAlias("post", "layouts/my_new_post_layout.njk");
 
     // Date formatting (human readable)
     config.addFilter("readableDate", dateObj => {
